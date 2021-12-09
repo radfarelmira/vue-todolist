67fa1e908269d0b97922a1bb7f2117a27f1a75d9
radfarelmira <radfar.elmira@email.com>
2021-12-09 13:32:27 +0100

    scaffolding

---
+++ milestone-1/js/script.js
@@ -0,0 +1,21 @@
+// Rifare l'esercizio della to do list.
+// Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+// - text, una stringa che indica il testo del todo
+// - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+// MILESTONE 1
+// Stampare all'interno di una lista, un item per ogni todo.
+// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+
+
+Vue.config.devtools = true;
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            
+        },
+        methods: {
+           
+    }
+);
